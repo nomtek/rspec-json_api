75e3227ef8f05587a0c54c33c692c89721ab85d1
Michal Gajowiak <m.gajowiak@nomtek.com>
2021-08-24 15:03:13 +0200

    Add project scaffold

---
+++ lib/rspec/json_api.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "json_api/version"
+
+module Rspec
+  module JsonApi
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rspec/json_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rspec::JsonApi do
+  it "has a version number" do
+    expect(Rspec::JsonApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
