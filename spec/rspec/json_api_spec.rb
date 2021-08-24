1daaf0a8b6bb9aba8eeefdac2eb5dae46a80a861
Michal Gajowiak <m.gajowiak@nomtek.com>
2021-08-24 16:44:44 +0200

    Fix github CI config file

--- spec/rspec/json_api_spec.rb
+++ spec/rspec/json_api_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rspec::JsonApi do
   it "has a version number" do
     expect(Rspec::JsonApi::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
